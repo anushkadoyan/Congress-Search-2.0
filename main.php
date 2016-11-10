1a1cac92220bc99e15de95f1b68be994c234e552
Anush Kadoyan <anushkadoyan@gmail.com>
2016-11-10 11:30:23 -0800

    Can tab between legislators and bills

--- main.php
+++ main.php
@@ -15,11 +15,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 /* get all legislators without pagination */
-$legislators = request("http://congress.api.sunlightfoundation.com/legislators?per_page=all&fields=party,first_name,last_name,chamber,district,state&apikey=9d713eee2bda4febb053035ef76e5f4c");
-$committees = request("http://congress.api.sunlightfoundation.com/committees?per_page=all&fields=chamber,committee_id,name,parent_committee_id,phone&apikey=9d713eee2bda4febb053035ef76e5f4c");
+$legislators = request("http://104.198.0.197:8080/legislators?per_page=all&fields=party,first_name,last_name,chamber,district,state&apikey=9d713eee2bda4febb053035ef76e5f4c");
+$committees = request("http://104.198.0.197:8080/committees?per_page=all&fields=chamber,committee_id,name,parent_committee_id,phone&apikey=9d713eee2bda4febb053035ef76e5f4c");
 //&fields=bill_id,bill_type,chamber,introduced_on,official_title,sponsor
-$billsOld = request("http://congress.api.sunlightfoundation.com/bills?per_page=50&history.active=true&fields=bill_id,bill_type,chamber,introduced_on,official_title,sponsor&apikey=9d713eee2bda4febb053035ef76e5f4c");
-$billsNew = request("http://congress.api.sunlightfoundation.com/bills?per_page=50&history.active=false&fields=bill_id,bill_type,chamber,introduced_on,official_title,sponsor&apikey=9d713eee2bda4febb053035ef76e5f4c");
+$billsOld = request("http://104.198.0.197:8080/bills?per_page=50&history.active=true&fields=bill_id,bill_type,chamber,introduced_on,official_title,sponsor&apikey=9d713eee2bda4febb053035ef76e5f4c");
+$billsNew = request("http://104.198.0.197:8080/bills?per_page=50&history.active=false&fields=bill_id,bill_type,chamber,introduced_on,official_title,sponsor&apikey=9d713eee2bda4febb053035ef76e5f4c");
 $content = array($legislators,$committees,$billsOld,$billsNew);
 echo (json_encode($content));
 
